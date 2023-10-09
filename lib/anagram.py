c4ecc263ef42ee21b58d5fc73334aee861762aed
Moses-Oyelade <oyelademoses@gmail.com>
2023-10-09 10:07:40 +0100

    all passsed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
 # your code goes here!
+
+word =["enlists", "google", "facebook", "inlets", "things"]
+
+
+class Anagram:
+    
+    def __init__(self, sent):
+        self.word = sent
+        
+    def match(self, word_list):
+        sorted_word = sorted(self.word)
+        return [word for word in word_list if sorted(word) == sorted_word]
+        
+        
+        
+        
+
+
+listen = Anagram("facebook")
+print(listen.match(word))
